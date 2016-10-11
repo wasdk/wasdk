eb14c58a927fa50ba0ca918f31bc2698e5b03b21
Michael Bebenita <mbebenita@gmail.com>
2016-10-11 16:28:08 -0700

    Download all tools.

--- get-tools.ts
+++ get-tools.ts
@@ -12,15 +12,20 @@ var Gauge = require("gauge");
 var rimraf = require('rimraf');
 var mkdirp = require('mkdirp');
 var tmp = require('tmp');
-var Promise = require('promise');
+var Promise = require('es6-promise');
 var decompress = require('decompress');
 var spawn = require('child_process').spawnSync;
+var logSymbols = require('log-symbols');
 
-let baseUrl = "http://people.mozilla.org/~ydelendik/tmp/llvm-wasm-tiny.tar.gz";
+let baseUrl = "http://people.mozilla.org/~ydelendik/tmp/llvm-binaryen-mac.tar.gz";
 // let baseUrl = "http://google.com/doodle.png";
-let llvmRootDirectory = "./bin/llvm";
-let clangPath = llvmRootDirectory + "/bin/clang-4.0";
-let llcPath = llvmRootDirectory + "/bin/llc";
+let binDirectory = "./bin";
+let clangPath = binDirectory + "/clang-4.0";
+let llcPath = binDirectory + "/llc";
+let s2wasmPath = binDirectory + "/s2wasm";
+let wasmAsPath = binDirectory + "/wasm-as";
+let wasmDisPath = binDirectory + "/wasm-dis";
+
 var gauge = new Gauge();
 
 declare var Math: any;
@@ -39,7 +44,7 @@ function downloadFile(url: string, path: string): Promise<string> {
       delay: 1000
     })
       .on('progress', function (state) {
-        gauge.show(`Downloading LLVM`, state.percentage);
+        gauge.show(`Downloading Tools`, state.percentage);
       })
       .on('error', function (err) {
         gauge.hide();
@@ -62,37 +67,70 @@ function check() {
   let cppFile = tmp.fileSync({ postfix: ".cpp" }).name;
   let bcFile = tmp.fileSync({ postfix: ".bc" }).name;
   let sFile = tmp.fileSync({ postfix: ".s" }).name;
+  let wastFile = tmp.fileSync({ postfix: ".wast" }).name;
+  let wasmFile = tmp.fileSync({ postfix: ".wasm" }).name;
   var child = spawn(clangPath, ['--version']);
-  let args;
+  // Check clang
   if (child.stdout.indexOf("clang version 4.0.0") < 0) {
     fail("Cannot validate clang executable.");
   }
+  fs.writeFileSync(cppFile, "int main() { return 42; }", 'utf8');
+  child = spawn(clangPath, ["--target=wasm32", "-O0", cppFile, "-c", "-emit-llvm", "-o", bcFile]);
+  if (child.status != 0) {
+    fail(child.stderr);
+  } else {
+    log.info("clang " + logSymbols.success);
+  }
+
+  // Check llc
   child = spawn(llcPath, ['--version']);
   if (child.stdout.indexOf("LLVM version 4.0.0") < 0) {
     fail("Cannot validate llc executable.");
+  } else {
+    log.info("llc " + logSymbols.success);
   }
-  fs.writeFileSync(cppFile, "int main() { return 42; }", 'utf8');
-  args = ["--target=wasm32", "-O0", cppFile, "-c", "-emit-llvm", "-o", bcFile];
-  child = spawn(clangPath, args);
-  if (child.status != 0) {
-    fail(child.stderr);
-  }
-  args = ["-o", sFile, bcFile];
-  child = spawn(llcPath, args);
+  child = spawn(llcPath, ["-o", sFile, bcFile]);
   if (child.status != 0) {
     fail(child.stderr);
   }
   let sFileData = fs.readFileSync(sFile);
   if (sFileData.indexOf("main:") < 0) {
     fail("Something went wrong when compiling to .s file.");
+  } else {
+    log.info("llc " + logSymbols.success);
   }
+
+  // Check s2wasm
+  child = spawn(s2wasmPath, [sFile, "-o", wastFile]);
+  if (child.status != 0) {
+    fail(child.stderr);
+  }
+  let wastFileData = fs.readFileSync(wastFile);
+  if (wastFileData.indexOf("(module") < 0) {
+    fail("Something went wrong when compiling to .wast file.");
+  } else {
+    log.info("s2wasm " + logSymbols.success);
+  }
+
+  // Check wasm-as
+  child = spawn(wasmAsPath, [wastFile, "-o", wasmFile]);
+  if (child.status != 0) {
+    fail(child.stderr);
+  }
+  if (!fs.existsSync(wasmFile)) {
+    fail("Something went wrong when compiling to .wasm file.");
+  } else {
+    log.info("wasm-as " + logSymbols.success);
+  }
+
+  // Check s2wasm
   log.info("All appears to be working.");
 }
 
 function downloadFilesAndCheck() {
   downloadFile(baseUrl, tmp.fileSync({ postfix: ".tar.gz" }).name).then((path) => {
-    gauge.show(`Unpacking LLVM`);
-    decompress(path, llvmRootDirectory).then(function (files) {
+    gauge.show(`Unpacking Tools`);
+    decompress(path, binDirectory).then(function (files) {
       if (files.length == 0) {
         fail(`Cannot unpack file ${path}`);
       } else {
@@ -109,13 +147,13 @@ function downloadFilesAndCheck() {
 let clean = false;
 
 if (clean) {
-  rimraf.sync(llvmRootDirectory);
+  rimraf.sync(binDirectory);
 }
 
 if (fs.existsSync(clangPath) && fs.existsSync(llcPath)) {
   check();
 } else {
-  mkdirp(llvmRootDirectory, function () {
+  mkdirp(binDirectory, function () {
     downloadFilesAndCheck();
   });
 }
