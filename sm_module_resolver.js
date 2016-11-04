abd4d49b1415353e5887b7a023500807871ab084
Yury Delendik <ydelendik@mozilla.com>
2016-11-04 16:24:35 -0500

    Fix sm_module_resolver for new npm.

--- sm_module_resolver.js
+++ sm_module_resolver.js
@@ -17,16 +17,21 @@
 // Helper file to mock CommonJS require() loader.
 var require = function (name) {
   var module = {exports: {}};
-  try {
-    var prefix = "./node_modules/" + name + "/";
-    var packageJsonPath = prefix + "package.json";
-    var packageJson = JSON.parse(readRelativeToScript(packageJsonPath));
-    var mainPath = prefix + packageJson.main;
-    var body = readRelativeToScript(mainPath);
-    var fn = new Function("module", "exports", "require", body);
-    fn.call(module, module, module.exports, require);
-    return module.exports;
-  } catch (e) {
+  var found = ['./node_modules', '../../node_modules'].some(prefix => {
+    try {
+      prefix = prefix + "/" + name + "/";
+      var packageJsonPath = prefix + "package.json";
+      var packageJson = JSON.parse(readRelativeToScript(packageJsonPath));
+      var mainPath = prefix + packageJson.main;
+      var body = readRelativeToScript(mainPath);
+      var fn = new Function("module", "exports", "require", body);
+      fn.call(module, module, module.exports, require);
+      return true;
+    } catch(_) {
+      return false;
+    }
+  });
+  if (!found)
     throw new Error('Module ' + name + ' was not found: ' + e);
-  }
+  return module.exports;
 };
