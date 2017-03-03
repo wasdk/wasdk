244f6596370aedb2bfba693527abebfbac661861
Yury Delendik <ydelendik@mozilla.com>
2017-03-03 13:43:46 -0600

    Move utils to wasdk-utils

--- shared.ts
+++ shared.ts
@@ -15,22 +15,24 @@
  */
 
 import * as fs from "fs";
-import * as tmp from "tmp";
 import * as path from "path";
-import * as http from "http";
-import * as https from "https";
 
-var rimraf = require('rimraf');
-var mkdirp = require('mkdirp');
-var logSymbols = require('log-symbols');
-var spawnSyncFn = require('child_process').spawnSync;
+import {
+  setVerbose, replaceBackslash, spawnDownloadFileSync, ifWindows,
+  pathLooksLikeDirectory, downloadFileSync as downloadFileSync_utils
+} from 'wasdk-utils';
+
+export {
+  appendFilesSync, spawnSync, fail, pathLooksLikeDirectory, endsWith,
+  flatten, createTmpFile, decompressFileSync, deleteFileSync,
+  ensureDirectoryCreatedSync, TMP_DIR
+} from 'wasdk-utils';
+
+export let WASDK_DEBUG = process.env.WASDK_DEBUG;
+setVerbose(WASDK_DEBUG);
 
 export const WASDK_ROOT = path.resolve(path.dirname(path.dirname(__filename)));
 
-export function spawnSync(command: string, args: any [], options?: any): any {
-  WASDK_DEBUG && console.error(command + " " + args.join(" "));
-  return spawnSyncFn(command, args, options);
-}
 export function pathFromRoot(...pathSegments: string []): string {
   pathSegments.unshift(WASDK_ROOT);
   return path.resolve.apply(path, pathSegments);
@@ -39,11 +41,7 @@ export function wasdkPath(pathSegment: string) {
   if (path.isAbsolute(pathSegment)) return pathSegment;
   return pathFromRoot(pathSegment);
 }
-export function ifWindows(s: string): string {
-  return process.platform === 'win32' ? s : '';
-}
 
-export let WASDK_DEBUG = process.env.WASDK_DEBUG;
 export let EM_CONFIG = pathFromRoot("bin", ".emscripten");
 export let LLVM_ROOT = pathFromRoot("bin", "llvm");
 export let EMSCRIPTEN_ROOT = pathFromRoot("bin", "emscripten");
@@ -54,37 +52,6 @@ export let EMCC = pathFromRoot("bin", "emscripten", "emcc" + ifWindows(".bat"));
 export let JS = pathFromRoot(SPIDERMONKEY_ROOT, "js" + ifWindows(".exe"));
 
 export let WEBIDL_BINDER = pathFromRoot("bin", "emscripten", "tools", "webidl_binder.py");
-// export let TMP_DIR = tmp.dirSync().name;
-export let TMP_DIR = pathFromRoot(".wasdk-tmp");
-
-// console.log(`TMP_DIR = ${TMP_DIR}`);
-
-export function fail(message) {
-  throw new Error(message)
-}
-export function endsWith(subjectString: string, searchString: string): boolean {
-  let position = subjectString.length;
-  position -= searchString.length;
-  var lastIndex = subjectString.lastIndexOf(searchString, position);
-  return lastIndex !== -1 && lastIndex === position;
-}
-export function flatten(elements: any [], target?: any []) {
-  if (!target) target = [];
-  elements.forEach(element => {
-    if (Array.isArray(element)) {
-      flatten(element, target);
-    } else {
-      target.push(element);
-    }
-  });
-  return target;
-}
-export function replaceBackslash(s: string): string {
-  return s.split('\\').join('/');
-}
-export function doubleBackslash(s: string): string {
-  return s.split('\\').join('\\\\');
-}
 
 export function writeEMConfig() {
   let str = `LLVM_ROOT = '${replaceBackslash(LLVM_ROOT)}'
@@ -95,32 +62,12 @@ COMPILER_ENGINE = NODE_JS
 JS_ENGINES = [NODE_JS]`;
   fs.writeFileSync(EM_CONFIG, str);
 }
-export function appendFilesSync(output: string, input: string [], insertNewLine = false) {
-  let files = input.map(file => fs.readFileSync(file, 'utf8'));
-  files.forEach(file => {
-    fs.appendFileSync(output, file);
-    if (insertNewLine) {
-      fs.appendFileSync(output, "\n");
-    }
-  });
-}
+
 function element<T>(array: T[], i): T {
   if (i >= 0) return array[i];
   return array[array.length + i];
 }
-export function pathLooksLikeDirectory(path: string) {
-  if (path === ".") return true;
-  let lastSlash = Math.max(path.lastIndexOf('/'), path.lastIndexOf('\\'));
-  let lastDot = path.lastIndexOf('.');
-  let noSuffix = lastDot < lastSlash || lastDot === -1;
-  if (noSuffix) return true;
-  let suffix = path.substr(lastDot);
-  if (suffix === ".exe" || suffix == ".zip") return false;
-  return true;
-}
-export function ensureDirectoryCreatedSync(path: string) {
-  mkdirp.sync(path);
-}
+
 export function downloadFileSync(url: string, dstPath: string, downloadEvenIfExists = false, filenamePrefix = ""): string {
   let filename = filenamePrefix + element(url.split("/"), -1);
   if (pathLooksLikeDirectory(dstPath)) {
@@ -129,69 +76,5 @@ export function downloadFileSync(url: string, dstPath: string, downloadEvenIfExi
     filename = dstPath;
   }
   filename = wasdkPath(filename);
-  mkdirp.sync(path.dirname(filename));
-  if (fs.existsSync(filename) && !downloadEvenIfExists) {
-    // console.log(`File ${filename} already downloaded, skipping.`);
-    console.log(`File already downloaded, skipping.`);
-    return filename;
-  }
-  // process.stdout.write(`Downloading ${url}: `);
-  process.stdout.write(`Downloading `);
-  if (spawnDownloadFileSync(url, filename)) return filename;
-}
-function spawnDownloadFileSync(url: string, filename: string): boolean {
-  let req = JSON.stringify({url, filename});
-  let res = spawnSync(process.execPath, [require.resolve('./download.js'), url, filename], {
-    stdio: [0, 1, 2]
-  });
-  if (res.status !== 0) fail(res.stderr.toString());
-  return fs.existsSync(filename);
-}
-export function decompressFileSync(filename: string, dstPath: string, strip = 0): boolean {
-  dstPath = wasdkPath(dstPath);
-  mkdirp.sync(dstPath);
-  process.stdout.write(`Unpacking`);
-  let res = spawnSync(process.execPath, [require.resolve('./unpack.js'), filename, dstPath, strip], {
-    stdio: [0, 1, 2]
-  });
-  if (res.status !== 0) fail(res.stderr.toString());
-  return true;
-}
-export function deleteFileSync(filename: string) {
-  filename = wasdkPath(filename);
-  rimraf.sync(filename);
-}
-export function createTmpFile(): string {
-  return tmp.fileSync({template: `${TMP_DIR}/tmp-XXXXXX`}).name;
-}
-
-export class IndentingWriter {
-  w: fs.WriteStream;
-  i: number;
-  constructor(path: string) {
-    this.i = 0;
-    this.w = fs.createWriteStream(path);
-  }
-  getIndent(): string {
-    let s = "";
-    let i = 0;
-    while (i++ < this.i) {
-      s += "  ";
-    }
-    return s;
-  }
-  writeLn(s: string) {
-    this.w.write(this.getIndent() + s + "\n");
-  }
-  enter(s: string) {
-    this.writeLn(s);
-    this.i++;
-  }
-  leave(s: string) {
-    this.i--;
-    this.writeLn(s);
-  }
-  end() {
-    this.w.end();
-  }
+  if (downloadFileSync_utils(url, filename, downloadEvenIfExists)) return filename;
 }
